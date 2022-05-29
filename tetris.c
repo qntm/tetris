dcc5f792f22f22bf257c17277d5ef3a3b216f4fe
qntm <qntm@users.noreply.github.com>
2022-05-29 21:15:03 +0100

    Backport linting

--- tetris.c
+++ tetris.c
@@ -196,7 +196,6 @@ struct State down(struct State state) {
 
 /* try to rotate the piece */
 struct State rotate(struct State state) {
-	short x, y;
 	struct State newP;
 
 	short newOrientationId = state.orientationId + 1;
@@ -240,7 +239,6 @@ struct State * startPtrs[NUMPIECES];
 distinguishing features are piece ID, orientation, xPos and yPos.
 Everything else is derived. */
 short equal(struct State p1, struct State p2) {
-	short maskId;
 	if(p1.pieceId != p2.pieceId) {
 		return 0;
 	}
@@ -271,12 +269,12 @@ struct State * find(struct State state) {
 /* show details about this state */
 void print(struct State * statePtr) {
 	short y, keyId;
-	printf("I am piece %c in orientation %d @ %d\n", pieceNames[statePtr->pieceId], statePtr->orientationId, statePtr);
+	printf("I am piece %c in orientation %d @ %p\n", pieceNames[statePtr->pieceId], statePtr->orientationId, statePtr);
 	printf("(xPos, yPos) is (%d, %d)\n", statePtr->xPos, statePtr->yPos);
 	printf("(xOffset, yOffset) is (%d, %d)\n", statePtr->xOffset, statePtr->yOffset);
 	printf("(xDim, yDim) is (%d, %d)\n", statePtr->xDim, statePtr->yDim);
 	for(keyId = 0; keyId < NUMCONTROLS; keyId++) {
-		printf("nextPtrs[%d] is %d\n", keyId, statePtr->nextPtrs[keyId]);
+		printf("nextPtrs[%d] is %p\n", keyId, statePtr->nextPtrs[keyId]);
 	}
 	for(y = statePtr->yTop; y < statePtr->yBottom; y++) {
 		printf("grid[%d] is %d\n", y, statePtr->grid[y]);
@@ -518,7 +516,7 @@ immediate line (or, if the player plays rationally afterwards, an eventual
 guaranteed line) and return a pointer to that state. If no such state can be
 found, return NULL. */
 struct State * getMagicLandingPtr(struct State * statePtr) {
-	short stateId, j, y, numLandings = 0;
+	short stateId, y, numLandings = 0;
 
 	// show(statePtr);
 	// printf("Finding the rational landing state for this piece\n");
@@ -605,7 +603,7 @@ short getKillerPiece() {
 
 /* generate an exhaustive web of linked possible piece positions and
 orientations, then let the user navigate this web */
-void main(int argc, char ** argv) {
+void main(void) {
 	short result;
 	long start, end;
 
@@ -630,6 +628,6 @@ void main(int argc, char ** argv) {
 	} else {
 		printf("Depth %d: An evil AI can always kill you with no lines from this position, if it gives you piece %c\n", depth, pieceNames[result]);
 	}
-	printf("Processing took %d second(s)\n", end - start);
+	printf("Processing took %ld second(s)\n", end - start);
 	return 0;
 }
